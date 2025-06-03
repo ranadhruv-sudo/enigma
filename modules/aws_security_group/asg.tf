509d0f877a731295533f54df3c6f4e6d424051fa
Dhruv Rana <dhruv@qualityai.ca>
2025-06-03 13:41:36 -0400

    update egress

--- modules/aws_security_group/asg.tf
+++ modules/aws_security_group/asg.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.168.1.1/32"]
     description = "Allow all outbound traffic"
   }
 }
