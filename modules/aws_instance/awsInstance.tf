ed68dc4c69b9a43335d9fdf62f56b6d4cbd2c04f
Dhruv Rana <dhruv@qualityai.ca>
2025-06-04 07:38:53 -0400

    refactor: clean up whitespace in main.tf and awsInstance.tf; add outputs for aws_instance module

--- modules/aws_instance/awsInstance.tf
+++ modules/aws_instance/awsInstance.tf
@@ -1,10 +1,7 @@
 resource "aws_instance" "web" {
-  ami               = "ami-0c02fb55956c7d316" # Amazon Linux 2 (change as needed)
-  instance_type     = "t3.micro"
-  availability_zone = "us-east-1a"
-
-
-
+  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (change as needed)
+  instance_type          = "t3.micro"
+  availability_zone      = "us-east-1a"
   vpc_security_group_ids = var.security_group_ids
 
   metadata_options {
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "ec2_public_ip" {
-  value = module.aws_instance_module.ec2_public_ip
+output "aws_instance_public_ip" {
+    value = module.aws_instance_module.aws_instance_public_ip
+}
+
+output "aws_instance_public_dns" {
+    value = module.aws_instance_module.aws_instance_public_dns  
 }
