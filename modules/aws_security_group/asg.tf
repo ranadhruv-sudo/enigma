52d5759af19de9baae81b07cc989e2b1337b66bc
Dhruv Rana <dhruv@qualityai.ca>
2025-06-06 10:29:09 -0400

    change port number

--- modules/aws_security_group/asg.tf
+++ modules/aws_security_group/asg.tf
@@ -12,8 +12,8 @@ resource "aws_security_group" "web_sg" {
 
   ingress {
     description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
