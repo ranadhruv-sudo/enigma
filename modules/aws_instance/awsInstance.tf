c37156dd92393bcd126db4a161b0b779c1173ccd
Dhruv Rana <dhruv@qualityai.ca>
2025-06-06 10:57:23 -0400

    change ami

--- modules/aws_instance/awsInstance.tf
+++ modules/aws_instance/awsInstance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami                         = "ami-0c02fb55956c7d316" # Amazon Linux 2 (change as needed)
+  ami                         = "ami-002db26d4a4c670c1" 
   instance_type               = "t3.micro"
   availability_zone           = "us-east-1a"
   vpc_security_group_ids      = var.security_group_ids
